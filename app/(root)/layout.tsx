8719d323d8805136483acc5e17a0901f673e1c95
Akansha Panchal <akanshapanchal@Akanshas-MacBook-Air.local>
2026-02-16 13:42:10 +0530

    Initial refactor for home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,17 @@
 import type { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function RootGroupLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">Ace Interview</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Page = () => {
   return (
-    <div>
-      <h1>Home Page </h1>
-    </div>
+    <>
+    <section className='card-cta'>
+      <div className="flex flex-col gap-6">
+          <h2 className="">Get AI Interview ready with practice and feedback!</h2>
+          <p className="text-lg">Practice job interviews with AI and get feedback on your performance.</p>
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+      </div>
+      <Image src="/robot.png" alt="robot" width={500} height={500} className='max-sm:hidden'/>
+    </section>
+
+    <section className='flex flex-col gap-6 mt-8'>
+      <h2>Your Interviews</h2>
+      <div className='interviews-section'>
+        <p>You haven&apos;t taken any interviews yet.</p>
+      </div>
+    </section>
+
+    <section className='flex flex-col gap-6 mt-8'>
+      <h2>Take an Interview</h2>
+      <div className='interviews-section'>
+        <p>There are no interviews available at the moment.</p>
+      </div>
+    </section>
+    </>
   );
 };
 
